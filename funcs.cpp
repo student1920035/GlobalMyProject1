56865f71589190c0e212dc455c5cf990ffb2e5ee
Ekaterina <ekaterina.semenova.vl@gmail.com>
2021-10-26 08:16:52 +0300

    add function MyPow() version2

--- funcs.cpp
+++ funcs.cpp
@@ -1,20 +1,29 @@
-//funcs.cpp
-#include "stdafx.h"
-#include "funcs.h"
-double MySum(double a, double b)
-{
-return a + b;
-}
-double MySub(double a, double b)
-{
-return a - b;
-}
-double MyMul(double a, double b)
-{
-return a * b;
-}
-double MyDiv(double a, double b)
-{
-return a / b;
-}
-
+//funcs.cpp 
+#include "stdafx.h" 
+#include "funcs.h"  
+#include <cmath> 
+ 
+double MySum(double a, double b) 
+{ 
+ return a + b; 
+} 
+ 
+double MySub(double a, double b) 
+{ 
+ return a - b; 
+} 
+ 
+double MyMul(double a, double b) 
+{ 
+ return a * b; 
+} 
+ 
+double MyDiv(double a, double b) 
+{ 
+ return a / b; 
+} 
+ 
+double MyPow(double a, double b) 
+{ 
+ return exp(b * log(a)); 
+} 
--- program.cpp
+++ program.cpp
@@ -1,16 +1,16 @@
-//файл program.cpp
-#include "stdafx.h"
-#include <iostream>
-#include "funcs.h"
-using namespace std;
-int main()
-{
-cout << MySum(10, 3) << endl;
-cout << MySub(10, 3) << endl;
-cout << MyMul(10, 3) << endl;
-
-cout <<MyDiv(10, 3) <<endl;
-
-system("pause");
-return 0;
-}
+//файл program.cpp 
+#include "stdafx.h" 
+#include <iostream> 
+#include "funcs.h" 
+using namespace std; 
+ 
+int main() 
+{ 
+ cout << MySum(10, 3) << endl; 
+ cout << MySub(10, 3) << endl; 
+ cout << MyMul(10, 3) << endl; 
+ cout << MyDiv(10, 3) << endl; 
+ cout << MyPow(10.5, 3.7) << endl; 
+ system("pause"); 
+ return 0; 
+} 
