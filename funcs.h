5be288cfbf0618753eb1327299c3f79e615ead24
Ekaterina <ekaterina.semenova.vl@gmail.com>
2021-10-26 08:38:36 +0300

    Hi!

--- funcs.h
+++ funcs.h
@@ -1,7 +1,4 @@
-// заголовочный файл funcs.h  
- 
-#ifndef __FUNCS_H__  
- 
+//Hi! 
 #define __FUNCS_H__  
  
 double MySum(double, double); 
@@ -10,4 +7,4 @@ double MyMul(double, double);
 double MyDiv(double, double); 
 double MyPow(double, double); 
  
-#endif // __FUNCS_H_
+#endif // __FUNCS_H__
